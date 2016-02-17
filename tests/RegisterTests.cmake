19fd7d295ad6f7de0f9f56d713c493c75161d760
ud1 <ud1@mail.ru>
2016-02-17 23:48:18 +0300

    add buffer

--- tests/RegisterTests.cmake
+++ tests/RegisterTests.cmake
@@ -2,3 +2,4 @@
 cxx_test(PRETTY_NAME "core::network" BIN_NAME "test_network" SOURCES "toxcore/test_network.cpp" LIBS toxcore)
 cxx_test(PRETTY_NAME "core::crypto::core" BIN_NAME "test_crypto_core" SOURCES "toxcore/test_crypto_core.cpp" LIBS toxcore)
 cxx_test(PRETTY_NAME "core::crypto::ping_array" BIN_NAME "test_ping_array" SOURCES "toxcore/test_ping_array.cpp" LIBS toxcore)
+cxx_test(PRETTY_NAME "core::buffer" BIN_NAME "test_buffer" SOURCES "toxcore/test_buffer.cpp" LIBS toxcore)

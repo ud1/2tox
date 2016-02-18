7f2698ce0d0c0f23f1d0b862d4f1606a5b26a4b6
ud1 <ud1@mail.ru>
2016-02-18 22:48:19 +0300

    buffer code review fix

--- tests/toxcore/test_buffer.cpp
+++ tests/toxcore/test_buffer.cpp
@@ -3,56 +3,81 @@
 
 #include <algorithm>
 
-TEST(buffer, test_pub_key_and_nonce)
+TEST (buffer, test_pub_key_and_nonce)
 {
-	{
-        SCOPED_TRACE("Public key to bytes");
-		
-		bitox::OutputBuffer output_buffer;
-		bitox::PublicKey public_key;
-		bitox::Nonce nonce;
-		
-		uint8_t num = 0;
-		std::generate(public_key.data.begin(), public_key.data.end(), [&num](){return num++;});
-		
-		num = 100;
-		std::generate(nonce.data.begin(), nonce.data.end(), [&num](){return num++;});
-		
-		
-		output_buffer << public_key << nonce;
-		ASSERT_EQ(32 + 24, output_buffer.size());
-		
-		for (uint8_t i = 0; i < 32; ++i)
-		{
-			ASSERT_EQ(i, *(output_buffer.begin() + i));
-		}
-		
-		for (uint8_t i = 0; i < 24; ++i)
-		{
-			ASSERT_EQ(100 + i, *(output_buffer.begin() + i + 32));
-		}
-		
-		bitox::InputBuffer input_buffer(output_buffer.begin(), output_buffer.size());
-		bitox::PublicKey public_key2;
-		bitox::Nonce nonce2;
-		
-		input_buffer >> public_key2 >> nonce2;
-		ASSERT_FALSE(input_buffer.fail());
-		
-		uint8_t b;
-		input_buffer >> b;
-		ASSERT_TRUE(input_buffer.fail());
-		ASSERT_TRUE(input_buffer.eof());
-		
-		for (uint8_t i = 0; i < 32; ++i)
-		{
-			ASSERT_EQ(i, *(public_key2.data.begin() + i));
-		}
-		
-		for (uint8_t i = 0; i < 24; ++i)
-		{
-			ASSERT_EQ(100 + i, *(nonce2.data.begin() + i));
-		}
-	}
+    {
+        SCOPED_TRACE ("Public key to bytes");
+
+        bitox::OutputBuffer output_buffer;
+        bitox::PublicKey public_key;
+        bitox::Nonce nonce;
+
+        uint8_t num = 0;
+        std::generate (public_key.data.begin(), public_key.data.end(), [&num]()
+        {
+            return num++;
+        });
+
+        num = 100;
+        std::generate (nonce.data.begin(), nonce.data.end(), [&num]()
+        {
+            return num++;
+        });
+
+
+        output_buffer << public_key << nonce;
+        ASSERT_EQ (32 + 24, output_buffer.size());
+
+        for (uint8_t i = 0; i < 32; ++i)
+        {
+            ASSERT_EQ (i, * (output_buffer.begin() + i));
+        }
+
+        for (uint8_t i = 0; i < 24; ++i)
+        {
+            ASSERT_EQ (100 + i, * (output_buffer.begin() + i + 32));
+        }
+
+        bitox::InputBuffer input_buffer (output_buffer.begin(), output_buffer.size());
+        bitox::PublicKey public_key2;
+        bitox::Nonce nonce2;
+
+        input_buffer >> public_key2 >> nonce2;
+        ASSERT_FALSE (input_buffer.fail());
+
+        uint8_t b;
+        input_buffer >> b;
+        ASSERT_TRUE (input_buffer.fail());
+        ASSERT_TRUE (input_buffer.eof());
+
+        for (uint8_t i = 0; i < 32; ++i)
+        {
+            ASSERT_EQ (i, * (public_key2.data.begin() + i));
+        }
+
+        for (uint8_t i = 0; i < 24; ++i)
+        {
+            ASSERT_EQ (100 + i, * (nonce2.data.begin() + i));
+        }
+    }
+}
+
+TEST (pub_key_and_nonce, test_is_zero_initialized)
+{
+    {
+        SCOPED_TRACE ("Public key and nonce are zero initialized");
+        bitox::PublicKey public_key;
+        bitox::Nonce nonce;
+
+        for (uint8_t i = 0; i < 32; ++i)
+        {
+            ASSERT_EQ (0, * (public_key.data.begin() + i));
+        }
+
+        for (uint8_t i = 0; i < 24; ++i)
+        {
+            ASSERT_EQ (0, * (nonce.data.begin() + i));
+        }
+    }
 }
 
--- toxcore/buffer.hpp
+++ toxcore/buffer.hpp
@@ -10,132 +10,132 @@
 
 namespace bitox
 {
-	
+
 constexpr size_t PUBLIC_KEY_LEN = 32;
 constexpr size_t NONCE_LEN = 24;
-	
+
 struct PublicKey
 {
-	std::array<uint8_t, PUBLIC_KEY_LEN> data;
+    std::array<uint8_t, PUBLIC_KEY_LEN> data = {};
 };
 
 struct Nonce
 {
-	std::array<uint8_t, NONCE_LEN> data;
+    std::array<uint8_t, NONCE_LEN> data = {};
 };
 
 class OutputBuffer
 {
 public:
-	
-	template<typename ByteIter>
-	void add_bytes(ByteIter begin, ByteIter end)
-	{
-		buffer.insert(buffer.end(), begin, end);
-	}
-	
-	size_t size() const
-	{
-		return buffer.size();
-	}
-	
-	const uint8_t *begin() const
-	{
-		return buffer.data();
-	}
-	
-	const uint8_t *end() const
-	{
-		return begin() + size();
-	}
-	
+
+    template<typename ByteIter>
+    void add_bytes (ByteIter begin, ByteIter end)
+    {
+        buffer.insert (buffer.end(), begin, end);
+    }
+
+    size_t size() const
+    {
+        return buffer.size();
+    }
+
+    const uint8_t *begin() const
+    {
+        return buffer.data();
+    }
+
+    const uint8_t *end() const
+    {
+        return begin() + size();
+    }
+
 private:
-	std::vector<uint8_t> buffer;
+    std::vector<uint8_t> buffer;
 };
-	
+
 inline OutputBuffer &operator << (OutputBuffer &buffer, const PublicKey &public_key)
 {
-	buffer.add_bytes(public_key.data.begin(), public_key.data.end());
-	return buffer;
+    buffer.add_bytes (public_key.data.begin(), public_key.data.end());
+    return buffer;
 }
 
 inline OutputBuffer &operator << (OutputBuffer &buffer, const Nonce &nonce)
 {
-	buffer.add_bytes(nonce.data.begin(), nonce.data.end());
-	return buffer;
+    buffer.add_bytes (nonce.data.begin(), nonce.data.end());
+    return buffer;
 }
 
 inline OutputBuffer &operator << (OutputBuffer &buffer, uint8_t b)
 {
-	buffer.add_bytes(&b, &b + 1);
-	return buffer;
+    buffer.add_bytes (&b, &b + 1);
+    return buffer;
 }
 
 class InputBuffer
 {
 public:
-	explicit InputBuffer(const uint8_t *buf, size_t size)
-	{
-		buffer.insert(buffer.end(), buf, buf + size);
-	}
-	
-	template<typename ByteIter>
-	void read_bytes(ByteIter *out, size_t count)
-	{
-		if (fail_bit || eof() || count > size())
-		{
-			fail_bit = true;
-			return;
-		}
-		
-		fail_bit = false;
-		std::copy(buffer.begin() + offset, buffer.begin() + offset + count, out);
-		offset += count;
-	}
-	
-	size_t size() const
-	{
-		return buffer.size() - offset;
-	}
-	
-	void reset()
-	{
-		fail_bit = false;
-	}
-	
-	bool eof() const
-	{
-		return !size();
-	}
-	
-	bool fail() const
-	{
-		return fail_bit;
-	}
-	
+    explicit InputBuffer (const uint8_t *buf, size_t size)
+    {
+        buffer.insert (buffer.end(), buf, buf + size);
+    }
+
+    template<typename ByteIter>
+    void read_bytes (ByteIter *out, size_t count)
+    {
+        if (fail_bit || eof() || count > size())
+        {
+            fail_bit = true;
+            return;
+        }
+
+        fail_bit = false;
+        std::copy (buffer.begin() + offset, buffer.begin() + offset + count, out);
+        offset += count;
+    }
+
+    size_t size() const
+    {
+        return buffer.size() - offset;
+    }
+
+    void reset()
+    {
+        fail_bit = false;
+    }
+
+    bool eof() const
+    {
+        return !size();
+    }
+
+    bool fail() const
+    {
+        return fail_bit;
+    }
+
 private:
-	std::vector<uint8_t> buffer;
-	size_t offset = 0;
-	
-	bool fail_bit = false;
+    std::vector<uint8_t> buffer;
+    size_t offset = 0;
+
+    bool fail_bit = false;
 };
 
 inline InputBuffer &operator >> (InputBuffer &buffer, PublicKey &public_key)
 {
-	buffer.read_bytes(public_key.data.begin(), public_key.data.size());
-	return buffer;
+    buffer.read_bytes (public_key.data.begin(), public_key.data.size());
+    return buffer;
 }
 
 inline InputBuffer &operator >> (InputBuffer &buffer, Nonce &nonce)
 {
-	buffer.read_bytes(nonce.data.begin(), nonce.data.size());
-	return buffer;
+    buffer.read_bytes (nonce.data.begin(), nonce.data.size());
+    return buffer;
 }
 
 inline InputBuffer &operator >> (InputBuffer &buffer, uint8_t &b)
 {
-	buffer.read_bytes(&b, 1);
-	return buffer;
+    buffer.read_bytes (&b, 1);
+    return buffer;
 }
 
 }
